306f05e10d814fb14c2f221cd20d382fd06c7239
Elyakub <muhammadabdulrahman437@gmail.com>
2023-10-24 10:40:51 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+var name = prompt("enter you name");
+if (name != null && name.length !== 0) {
+   // do your stuff here
+    
+   document.write("<h1><b>Hello </b></h>" + name);
+   
+
+}
+alert(name);
